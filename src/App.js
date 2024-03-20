bb6716f63c587ff0ae460cc8a25aef8ac809fefc
Cheta008 <destinychukwuma8@gmail.com>
2024-03-20 15:23:45 -0700

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import Weather from './Weather';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Cheta🦋💙
-      </h1>
+      <div className='container'>
+     <Weather />
+     <footer>
+      This project was coded by{" "}
+      <a href='https://github.com/Cheta008'>Chukwuma Destiny</a> and is{" "}
+      <a href='https://github.com/Cheta008/react-weather-app'>open-sourced on GitHub</a>{" "}and 
+      <a href='https://master--unique-naiad-e73a78.netlify.app/'>hosted on Netlify</a>
+     </footer>
+      </div>
     </div>
   );
 }
